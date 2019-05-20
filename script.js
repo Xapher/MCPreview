ee711922c4f5bd35ba0a830c4afd17ea0e363934
Xapher <schreckengostb1@gmail.com>
2019-05-20 14:44:38 -0700

    styling

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function load() {
-	var buttonString = "<button id=MCStyle></button>";
+	var buttonString = "<button id=MCStyle style=\"width:" + (window.innerWidth * .8) * .02 + "px; height:" + (window.innerWidth * .8) * .02 + "px;\"></button>";
+	var n = ((window.innerWidth * .8) * .02) / 100;
 	var divString = "";
 	for (var i = 0; i < 100; i++) {
 		divString =  divString + buttonString;	
 	}
+	//window.innerWidth
 	document.getElementById("grid").innerHTML = divString;
+	var buttons = document.getElementsByTagName('buton');
+	console.log(buttons);
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].style.width = window.innerWidth + "px";
+	}
+	
 }
