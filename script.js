13b0654676c188fab106c5ec21ede8fe43bab4b9
Xapher <schreckengostb1@gmail.com>
2019-05-21 01:47:33 +0000

    Update script.js
    More stuffs

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function load() {
-	var buttonString = "<button id=MCStyle style=\"width:" + (window.innerWidth * .8) * .02 + "px; height:" + (window.innerWidth * .8) * .02 + "px;\"onclick=changeBlock(";
+	var buttonStringStart = "<button id=";
+        var buttonMiddle= "style=\"width:" + (window.innerWidth * .8) * .02 + "px; height:" + (window.innerWidth * .8) * .02 + "px;\"onclick=changeBlock(";
         var buttonStringEnd = ")></button>";
 	var n = ((window.innerWidth * .8) * .02) / 100;
 	var divString = "";
         var id = "mcButton";
 	for (var i = 0; i < 100; i++) {
-		divString =  divString + (buttonString + id + i + buttonStringEnd);	
+		divString =  divString + (buttonStringStart + (id + i) + buttonMiddle + id + i + buttonStringEnd);	
 	}
 	//window.innerWidth
 	document.getElementById("grid").innerHTML = divString;
